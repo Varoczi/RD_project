84046ba6a223895ba37ebdbf45b3d3876bed8d35
Varoczi <varoczigergo@gmail.com>
2024-02-12 00:43:11 +0100

    Near_end

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matc
 } else {
     document.body.classList.add('dark-theme');
 };
+
+window.addEventListener('resize', function() {
+    shortenText();
+  });
+
+  function shortenText() {
+    var screenWidth = window.innerWidth;
+    var textElement = document.getElementById('header');
+
+    if (screenWidth <= 375) {
+      textElement.textContent = "ACME";
+    } else {
+      textElement.textContent = "ACME Inc.";
+    }
+  }
+  shortenText();
